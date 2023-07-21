f0b5b83cdf4cf2062d4b1c6104d9c8016480112b
Kevin Lai <kevinywlai@gmail.com>
2023-07-21 14:56:18 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+      <b-modal id="modal-1" title="BootstrapVue">
+      <p class="my-4">Hello from modal!</p>
+  </b-modal>
+</div>
   </div>
 </template>
 
