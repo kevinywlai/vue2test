bea019fe07aa68d23b03b298bb0cb802841572bb
kevinywlai <kevinywlai@gmail.com>
2023-07-22 16:23:41 +0800

    initial commit

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+```shell
+npm install vue-cli -g
+```
--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
       <b-button v-b-modal.modal-1>Launch demo modal</b-button>
 
       <b-modal id="modal-1" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
-  </b-modal>
+          <p class="my-4">Hello from modal!</p>
+      </b-modal>
 </div>
   </div>
 </template>
